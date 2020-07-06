7d8e9a2492bbca3ba0e685794f374583683da999
Krishan27 <ks2616459@gmail.com>
2020-07-05 19:53:13 -0500

    herokuagain

--- server/index.js
+++ server/index.js
@@ -18,6 +18,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+
+app.get("/",(req,res)=>{
+    res.json({"hello":"I am Krishna"})
+})
+
 app.get('/api/user/auth', auth , (req,res) =>{
     res.status(200).json({
         _id:req._id,
