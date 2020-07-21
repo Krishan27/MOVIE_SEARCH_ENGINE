e3bab5691d8d1e950ac8812dde67c59d13ffff3c
Krishan27 <ks2616459@gmail.com>
2020-07-21 11:50:26 -0500

    last  heroku

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,3 +1,3 @@
-module.exports ={
-    mongoURI: 'mongodb+srv://KrishanSharma:Northwest@123@cluster0-briyc.mongodb.net/test?retryWrites=true&w=majority'
+module.exports = {
+    mongoURI:process.env.MONGO_URI
 }
